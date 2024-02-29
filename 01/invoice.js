1b6f1b850f0d7309c6c3cb38700b069358f2f666
r-omatsu <matsumoto.ryoya1123@gmail.com>
2024-02-29 20:54:17 +0900

    1 totalAmount変数のインライン化、appleSauceメソッドの命名変更 - 本当はそれぞれのステップでコンパイル、テスト、コミットすべき

--- 01/invoice.js
+++ 01/invoice.js
@@ -7,13 +7,12 @@ function statement(invoice, plays) {
       perf.audience
     } seats)\n`;
   }
-  let totalAmount = appleSauce();
 
-  result += `Amount owed is ${usd(totalAmount)}\n`;
+  result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
   return result;
 
-  function appleSauce() {
+  function totalAmount() {
     let totalAmount = 0;
     for (let perf of invoice.performances) {
       totalAmount += amountFor(perf);

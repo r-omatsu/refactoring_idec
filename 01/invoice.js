32105c92b159827446f50a5a7122f9f7d564721e
r-omatsu <matsumotoryoya@mba.local>
2024-02-29 20:00:10 +0900

    1 ボリュームポイントの計算部分を切り出す - ローカルスコープの変数をできるだけ無くしたことで、関数の抽出がやりやすくなっている

--- 01/invoice.js
+++ 01/invoice.js
@@ -9,11 +9,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
     result += `  ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
@@ -51,6 +47,16 @@ function statement(invoice, plays) {
 
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type) {
+      result += Math.floor(aPerformance.audience / 5);
+    }
+
+    return result;
   }
 }
 

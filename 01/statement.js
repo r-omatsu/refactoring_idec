82f78b5a959fb847d6c3ba51739f67260f83951d
r-omatsu <matsumoto.ryoya1123@gmail.com>
2024-03-02 14:27:29 +0900

    1 計算とフォーマットのフェーズを分割 - html用のメソッドを追加した - html用のコードを除いてもコード量は増えているが、ロジックは認識しやすくなり、請求の計算処理部分と請求書のフォーマット作成部分に完璧に分かれている - プログラミングでもキャンプ場と同じルールを適用して、最初より綺麗にするというマインドが大切

--- 01/statement.js
+++ 01/statement.js
@@ -3,6 +3,9 @@ import createStatementData from "./createStatementData.js";
 export function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
+export function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
 
 function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`;
@@ -17,12 +20,28 @@ function renderPlainText(data, plays) {
   result += `Amount owed is ${usd(data.totalAmount)}\n`;
   result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
+}
 
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
+function renderHtml(data) {
+  let result = `<h1>Statement for ${data.customer}</h1>`;
+  result += "<table>\n";
+  result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+  for (let perf of data.performances) {
+    result += `    <tr><td>${perf.play.name}</td><td>${
+      perf.audience
+    }</td><td>${usd(perf.amount)}</td></tr>`;
   }
+  result += "</table>\n";
+  result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
 }

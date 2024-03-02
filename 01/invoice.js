22fb37f0aafbd59488feb7d032d67c92cdcfadcc
r-omatsu <matsumotoryoya@mba.local>
2024-03-02 14:27:29 +0900

    1 計算とフォーマットのフェーズを分割 - html用のメソッドを追加した - html用のコードを除いてもコード量は増えているが、ロジックは認識しやすくなり、請求の計算処理部分と請求書のフォーマット作成部分に完璧に分かれている - プログラミングでもキャンプ場と同じルールを適用して、最初より綺麗にするというマインドが大切

--- 01/invoice.js
+++ 01/invoice.js
@@ -1,6 +1,7 @@
 import fs from "fs";
-import { statement } from "./statement.js";
+import { htmlStatement, statement } from "./statement.js";
 
 const invoiceData = JSON.parse(await fs.readFileSync("./01/invoice.json"));
 const playsData = JSON.parse(await fs.readFileSync("./01/plays.json"));
 console.log(statement(invoiceData, playsData));
+console.log(htmlStatement(invoiceData, playsData));

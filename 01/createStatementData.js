47f843eb893c7fa08cb1a4cf4b430d68b1a5c3af
r-omatsu <matsumoto.ryoya1123@gmail.com>
2024-03-04 22:35:53 +0900

    1 ポリモーフィックにする - 喜劇もポリモーフィズムに対応 - PerformanceCalculatorのamountメソッドに墓石を立てた

--- 01/createStatementData.js
+++ 01/createStatementData.js
@@ -5,27 +5,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${this.performance.play.type}`);
-    }
-
-    return result;
+    throw "想定外の呼び出し";
   }
 
   get volumeCredits() {
@@ -95,4 +75,14 @@ class TragedyCalculator extends PerformanceCalculator {
   }
 }
 
-class ComedyCalculator extends PerformanceCalculator {}
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+
+    return result;
+  }
+}

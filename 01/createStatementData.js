bf213918f78e82bee4e8a6aa7ea30adc27d57d52
r-omatsu <matsumoto.ryoya1123@gmail.com>
2024-03-04 22:29:44 +0900

    1 ポリモーフィックにする - 悲劇の料金計算をTragedyCalculatorに移動

--- 01/createStatementData.js
+++ 01/createStatementData.js
@@ -85,6 +85,14 @@ function createPerformanceCalculator(aPerformance, aPlay) {
   }
 }
 
-class TragedyCalculator extends PerformanceCalculator {}
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
 
 class ComedyCalculator extends PerformanceCalculator {}

260ef6ea18d3eacfd6ca1a8045eba3db3d401818
r-omatsu <matsumoto.ryoya1123@gmail.com>
2024-03-04 22:19:39 +0900

    1 ポリモーフィックにする - TragedyCalculatorとComedyCalculatorクラスを宣言   - extendsしているのが気に掛かる。interfaceの方がいいのでは？

--- 01/createStatementData.js
+++ 01/createStatementData.js
@@ -75,5 +75,16 @@ export default function createStatementData(invoice, plays) {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-  return new PerformanceCalculator(aPerformance, aPlay);
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`未知の演劇の種類: ${aPlay.type}`);
+  }
 }
+
+class TragedyCalculator extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}

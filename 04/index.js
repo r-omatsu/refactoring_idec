7e4dcf64d983223297c0a5945b4f5557556e2fe1
r-omatsu <matsumoto.ryoya1123@gmail.com>
2024-03-12 23:50:29 +0900

    4 テスト実装前準備 - 不足分を計算するshortfallメソッド（単純）と利益を計算するprofitメソッド（比較的複雑）を実装

--- 04/index.js
+++ 04/index.js
@@ -13,6 +13,35 @@ class Province {
     this._totalProduction += arg.production;
   }
 
+  get shortfall() {
+    return this._demand - this.totalProduction;
+  }
+
+  get profit() {
+    return this.demandValue - this.demandCost;
+  }
+
+  get demandCost() {
+    let remainingDemand = this.demand;
+    let result = 0;
+    this.producers
+      .sort((a, b) => a.cost - b.cost)
+      .forEach((p) => {
+        const contribution = Math.min(remainingDemand, p.production);
+        remainingDemand -= contribution;
+        result += contribution * p.cost;
+      });
+    return result;
+  }
+
+  get demandValue() {
+    return this.satisfiedDemand * this.price;
+  }
+
+  get satisfiedDemand() {
+    return Math.min(this._demand, this.totalProduction);
+  }
+
   get name() {
     return this._name;
   }
